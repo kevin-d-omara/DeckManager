e17f7cdc7a1055d89d0d6dd5ef05a49ffb63e0e2
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-25 18:06:49 -0700

    Display 42 cards in a spread

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -12,16 +12,31 @@ namespace DeckManager
 
         private void Start()
         {
+            Debug.Log("start creating deck");
             deck = new Deck(deckFile);
-            CreateObjectsFromCards();
+            Debug.Log("end creating deck");
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("start create objects from cards");
+                CreateObjectsFromCards();
+                Debug.Log("end create objects from cards");
+            }
         }
 
         private void CreateObjectsFromCards()
         {
+            var position = new Vector3(-24f, -8f, 0f);
             foreach (Card card in deck.Cards)
             {
-                var instance = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                var instance = Instantiate(cardPrefab, position, Quaternion.identity) as GameObject;
+                instance.name = card.Name;
                 instance.GetComponent<SpriteRenderer>().sprite = card.Image;
+
+                position += new Vector3(1f, 0f, 0f);
             }
         }
     }
