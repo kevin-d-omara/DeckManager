5b64b730c6cf4aa19a2534de48c5e83f661696ae
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-25 17:48:14 -0700

    Add deckfile -> card gameobject conversion

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeckManager
+{
+    public class Driver : MonoBehaviour
+    {
+        public string deckFile;
+        private Deck deck;
+        public GameObject cardPrefab;
+
+        private void Start()
+        {
+            deck = new Deck(deckFile);
+            CreateObjectsFromCards();
+        }
+
+        private void CreateObjectsFromCards()
+        {
+            foreach (Card card in deck.Cards)
+            {
+                var instance = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                instance.GetComponent<SpriteRenderer>().sprite = card.Image;
+            }
+        }
+    }
+}
--- Assets/Scripts/Pure C#/Deck.cs
+++ Assets/Scripts/Pure C#/Deck.cs
@@ -6,6 +6,65 @@ namespace DeckManager
 {
     public class Deck
     {
-        
+        public List<Card> Cards = new List<Card>();
+
+        /// <summary>
+        /// Create a deck from the cards contained in the file provided.
+        /// </summary>
+        /// <param name="pathToDeckFile"> Filename in "Decks\" directory (i.e. "Season1_Spawn.deck"
+        /// or "\Custom\HellOnEarth.deck").</param>
+        public Deck(string pathToDeckFile)
+        {
+            string[] lines = System.IO.File.ReadAllLines("Assets\\Decks\\" + pathToDeckFile);
+
+            var pathToCards = "Assets\\Cards\\" + lines[0];
+
+            for (int i = 1; i < lines.Length; ++i)
+            {
+                var pathToCard = pathToCards + lines[i] + ".jpg";
+                Sprite image = LoadNewSprite(pathToCard);
+                Cards.Add(new Card(lines[i], image));
+            }
+        }
+
+        /// <summary>
+        /// Load a PNG or JPG image from disk into a sprite.
+        /// </summary>
+        /// <param name="FilePath">Filename in "Cards\" (i.e. "Spawn\1 Zombicide\1.jpg").</param>
+        /// <param name="PixelsPerUnit">[Optional]</param>
+        /// <returns>Sprite with the specified image.</returns>
+        private Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f)
+        {
+            var sprite = new Sprite();
+            var texture = LoadTexture(FilePath);
+            sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+                new Vector2(0, 0), PixelsPerUnit);
+
+            return sprite;
+        }
+
+        /// <summary>
+        /// Load a PNG or JPG file from disk to a Texture2D.
+        /// </summary>
+        /// <param name="FilePath">Filename in "Cards\" (i.e. "Spawn\1 Zombicide\1.jpg").</param>
+        /// <returns>Texture2D with image, or null if operation fails.</returns>
+        private Texture2D LoadTexture(string FilePath)
+        {
+            Texture2D Tex2D;
+            byte[] FileData;
+
+            if (System.IO.File.Exists(FilePath))
+            {
+                FileData = System.IO.File.ReadAllBytes(FilePath);
+                Tex2D = new Texture2D(1, 1);
+
+                // Load the imagedata into the texture (size is set automatically).
+                if (Tex2D.LoadImage(FileData))
+                {
+                    return Tex2D;
+                }
+            }
+            return null;
+        }
     }
 }
